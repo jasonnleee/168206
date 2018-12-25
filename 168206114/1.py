8b54ddae6f734f281834869dfd935d219e735c2d
jasonnleee <46127339+jasonnleee@users.noreply.github.com>
2018-12-25 17:09:16 +0800

    Update 1.py

--- 168206114/1.py
+++ 168206114/1.py
@@ -1,1 +1,16 @@
+thief = ['A','B','C','D']
 
+print("小偷是：",end="")
+for s in thief:
+	i = s  
+	A=(i != 'A')
+	B=(i == 'D')
+	C=(i == 'B')
+	D=(i != 'D')
+	if (A + B + C + D == 1):
+		lists = [A,B,C,D]
+		print(i,end=" ")
+		print("\n说真话的是：",end="")
+		for i in range(len(lists)):
+			if lists[i]:
+				print(thief[i],end=" ")
