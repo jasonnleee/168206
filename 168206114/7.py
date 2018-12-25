8759f7e74cd13f33b77b81b11bbcb820b5b9f278
jasonnleee <46127339+jasonnleee@users.noreply.github.com>
2018-12-25 17:52:03 +0800

    Update 7.py

--- 168206114/7.py
+++ 168206114/7.py
@@ -1,42 +1,32 @@
-def indegree0(v,e): 
- 	if v==[]:
- 		return None
- 	tmp=v[:]
- 	'''寻找入度为0的，放在tmp中'''
- 	for i in e:
- 		if i[1] in tmp:
- 			tmp.remove(i[1])
- 	if tmp==[]:
- 		return -1
- 	'''删除入度为0 的链接线'''
- 	for t in tmp:
- 		for i in range(len(e)):
- 			if t in e[i]:
- 				e[i]='toDel' #占位，之后删掉
- 		#tmp.remove(t)
- 	
- 	if e:
- 		eset=set(e)
- 		eset.remove('toDel')
- 		e[:]=list(eset)
- 	
- 	if v:
- 		for t in tmp:
- 			v.remove(t)
- 	
- 	return tmp
-def topoSort(v,e):
- 	result=[]
- 	while True:
- 		nodes=indegree0(v,e)
- 		if nodes==None:
- 			break
- 		elif nodes==-1:
- 			print('there\'s a circle.')
- 			return None
- 		result.extend(nodes)
- 	return result
-v=['a','b','c','d','e']
-e=[('a','b'),('a','d'),('b','c'),('d','c'),('d','e'),('e','c')]
-res=topoSort(v,e)
-print(res)
+def Message(arr,left,eight):
+	mid = len(arr)/2
+	mid = int(mid)
+	if left == mid or mid == eight:
+		if int(arr[left-1]) > int(arr[eight-1]):
+			arr[left-1],arr[eight-1] = arr[eight-1],arr[left-1]
+		return arr
+	else:
+		arr1 = Message(arr[:mid],left,mid)
+		a = int(eight) - int(mid)
+		arr2 = Message(arr[mid:],left,a)
+		return sorf(arr1,arr2)
+
+def sorf(arr1,arr2):
+	arr = []
+	while len(arr1)!=0 and len(arr2)!=0:
+		if arr1[0] >= arr2[0]:
+			arr.append(arr2.pop(0))
+		else:
+			arr.append(arr1.pop(0))
+	if len(arr1)==0:
+		for i in range(0,int(len(arr2))):
+			arr.append(arr2[i])
+	else:
+		for i in range(0,int(len(arr1))):
+			arr.append(arr1[i])
+	return arr
+	
+arrge = [51,45,12,35,42,54,7,46]
+arrge2 = Message(arrge,1,int(len(arrge)))
+print(arrge)
+print(arrge2)
