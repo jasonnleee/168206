1131e18901e1c758fda41f97e337269840a6a953
jasonnleee <46127339+jasonnleee@users.noreply.github.com>
2018-12-25 17:18:40 +0800

    Create 3.py

---
+++ 168206114/3.py
@@ -0,0 +1,26 @@
+"""
+def sum (alist):
+	s = 0
+	if len(alist)==0:
+		return 0
+	else:
+		s += alist[0]
+		alist.remove(alist[0])
+		return s + sum(alist)
+"""
+"""
+def Sum(alist):
+	if len(alist) < 1:
+		return 0
+	else:
+		for i in alist:
+			aum += i
+			return sum
+"""
+def SUM(alist):
+	if len(alist)==0:
+		return 0
+	return alist[0] + SUM(alist[1:])
+alist = [2,3,6,4,5]
+s=sum(alist)
+print(s)
